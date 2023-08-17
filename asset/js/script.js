a1595172dff7b4b69866c75b65e632e88206a7ef
andrey F de castro <andreycastro.564@gmail.com>
2023-08-17 10:49:32 -0300

    feat : creat createCategory function

--- asset/js/script.js
+++ asset/js/script.js
@@ -7,15 +7,33 @@ class category {
 }
 
 class Product {
-constructor(id,name,price) {
-this.id = id;
-this.name = name;
-this.price = price
-this.category = category;
-}
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 }
 
-class CategoryService{
-constructor(){
+class CategoryService {
+    constructor() {
+        this.categories = [];
+        this.nextCategoryId = 0;
+    }
+    
+    addCategory(name) {
+    const id = this.nextCategoryId++;
+    const category = new Category(id, name);
+    this.categories.push(category);  
+    }
 }
+
+const categoryList = new CategoryService();
+
+function creatCategory(){
+const categoryname = "Doce";
+
+categoryList.addCategory(categoryName);
+
+console.log(categoryList.categories);
 }
